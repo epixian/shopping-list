63f157c473a2eff99a86f752c09df25003ce8072
epixian <epixian@epixian.com>
2020-05-18 15:48:48 -0400

    prevent only whitespace items

--- src/components/NewItem.vue
+++ src/components/NewItem.vue
@@ -19,6 +19,7 @@ export default {
   },
   methods: {
     addItem() {
+      this.item = this.item.trim();
       if (this.item !== '') {
         this.$store.commit('addItem', this.item);
         this.item = '';
